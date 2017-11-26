62484ac7220fa4f08922fc462fc6f5aa190f43b3
Yuichi Shiraishi <friend1ws@gmail.com>
2017-11-26 16:00:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python
-"""
-from distutils.core import setup
-
-setup(name='genomon_rna_gce',
-      version='0.1.0',
-      description='Python tools for executing Genomon RNA pipeline in Google Compute Engine.',
-      author='Yuichi Shiraishi',
-      author_email='friend1ws@gamil.com',
-      url='https://github.com/friend1ws/genomon_rna_gce',
-      package_dir = {'': 'lib'},
-      package_data={'': ['script/*']},
-      packages=['genomon_rna_gce'],
-      scripts=['genomon_rna_gce'],
-      license='MIT'
-     )
-"""
 
 from setuptools import setup, find_packages
 from codecs import open
