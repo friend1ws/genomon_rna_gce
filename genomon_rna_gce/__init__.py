3046a39b52e54b21ace40f406196fccf2fbae2ec
Yuichi Shiraishi <friend1ws@gmail.com>
2017-12-17 10:58:41 +0900

    Update __init__.py

--- genomon_rna_gce/__init__.py
+++ genomon_rna_gce/__init__.py
@@ -4,7 +4,7 @@ from genomon_rna_gce.run import run
 def main():
 
     aparser = argparse.ArgumentParser(prog = "genomon_rna_gce")
-    aparser.add_argument("--version", action = "version", version = "genomon_rna_gce-0.1.0")
+    aparser.add_argument("--version", action = "version", version = "genomon_rna_gce-0.2.0")
     aparser.add_argument("sample_conf_file", default = None, type = str, help = "Sample config file")
     aparser.add_argument("output_dir", default = None, type = str,
                          help = "Output directory for Google Cloud Storage")
